4c76f08b954877f0d526595242b09c641d141081
1002542303@qq.com <7893507meng>
2019-01-21 11:36:53 +0800

    init

---
+++ src/test/java/meng/learns/auth2/Auth2ApplicationTests.java
@@ -0,0 +1,17 @@
+package meng.learns.auth2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Auth2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
